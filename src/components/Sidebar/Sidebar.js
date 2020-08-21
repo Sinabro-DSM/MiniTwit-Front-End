e61f3389ad40dc004b3c66d15f874c2ec209fb78
Haerin <kim34458747@gmail.com>
2020-08-22 02:52:19 +0900

    profile_view commit

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,54 +1,50 @@
-import React, { useState } from 'react';
-import './../../assets/style/sidebar/sidebar.css'
-import './../../assets/style/main/index.css'
-import {profile, threeDot} from '../../assets/img'
+import React, { useState } from "react";
+import "./../../assets/style/sidebar/sidebar.css";
+import "./../../assets/style/main/index.css";
+import { profile, threeDot } from "../../assets/img";
 
 const Sidebar = () => {
-    const [showing, setShowing] = useState(false)
-    const onClickMoreButton = () => setShowing(!showing);
-    
-    if(!showing)
-    {
-        return (
-            <div className="mainContainer">
-                <aside className="sidebar">
-                    <div className="twitIcon"></div>
-                    <article className="sideBarIconContainer">
-                        <div>
-                            <img src={profile}></img>
-                            <h3>프로필</h3>
-                        </div>
-                        <div onClick={onClickMoreButton}>
-                            <img src={threeDot}></img>
-                            <h3>더보기</h3>
-                        </div>
-                    </article>
-                </aside>
-            </div>  
-        );
-    }
-    else
-    {
-        return (
-            <div className="mainContainer">
-                <aside className="sidebar">
-                    <div className="twitIcon"></div>
-                    <article className="sideBarIconContainer">
-                        <div>
-                            <img src={profile}></img>
-                            <h3>프로필</h3>
-                        </div>
-                        <div className="moreBox" onClick={onClickMoreButton}>
-                            <img src={threeDot}></img>
-                            <h3>더보기</h3>
-                            <button>로그아웃</button>
-                        </div>
-                    </article>
-                </aside>
-            </div>  
-        );
-    }
-    
+  const [showing, setShowing] = useState(false);
+  const onClickMoreButton = () => setShowing(!showing);
+
+  if (!showing) {
+    return (
+      <div className="mainContainer">
+        <aside className="sidebar">
+          <div className="twitIcon"></div>
+          <article className="sideBarIconContainer">
+            <div>
+              <img src={profile}></img>
+              <h3>프로필</h3>
+            </div>
+            <div onClick={onClickMoreButton}>
+              <img src={threeDot}></img>
+              <h3>더보기</h3>
+            </div>
+          </article>
+        </aside>
+      </div>
+    );
+  } else {
+    return (
+      <div className="mainContainer">
+        <aside className="sidebar">
+          <div className="twitIcon"></div>
+          <article className="sideBarIconContainer">
+            <div>
+              <img src={profile}></img>
+              <h3>프로필</h3>
+            </div>
+            <div className="moreBox" onClick={onClickMoreButton}>
+              <img src={threeDot}></img>
+              <h3>더보기</h3>
+              <button>로그아웃</button>
+            </div>
+          </article>
+        </aside>
+      </div>
+    );
+  }
 };
 
 export default Sidebar;
