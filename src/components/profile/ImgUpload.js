2d8085a5bdbe4bdb473eea5d9c9a3e33db3cca3b
Haerin <kim34458747@gmail.com>
2020-08-24 23:21:26 +0900

    change img

--- src/components/profile/ImgUpload.js
+++ src/components/profile/ImgUpload.js
@@ -7,26 +7,34 @@ const ImgUpload = () => {
 
     const config = {
       headers: {
-        "access-token": "",
+        "access-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ2NWZjZGJiNTZkNTE0NmQ4NjNmMTNiNTViMzQ3YjIxIiwiZW1haWwiOiJzZXVuZ2Jpbjk4NTBAZ21haWwuY29tIiwibmlja25hbWUiOiJ5c2IiLCJpYXQiOjE1OTgyNzY5MDAsImV4cCI6MTU5ODI3ODcwMH0.5CVmZeQDs7z1g_mTylhDzulDgheab-qO0QQefY0-2gI",
         "Content-type": "application/x-www-form-urlencoded",
       },
     };
-
-    const form = new FormData();
+    console.log(file.files[0]);
+    let form = new FormData();
     form.append("file", file.files[0]);
-    axios.post("", form, config);
+    axios
+      .put("http://3.34.197.24:3000/profile", form, config)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <div>
       <form
-        onClick={changeImg}
-        action=""
-        method="post"
-        enctype="multiple/form-data"
+        action="http://3.34.197.24:3000/profile"
+        method="put"
+        enctype="multipart/form-data"
       >
         <input multiple="multiple" id="file" name="file" type="file" />
       </form>
+      <button onClick={changeImg}>설정</button>
     </div>
   );
 };
