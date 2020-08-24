0e3aca36308e27b64e74033e205b0577c5d9c88a
Haerin <kim34458747@gmail.com>
2020-08-25 03:57:46 +0900

    user_search

--- src/components/userSearch/search.js
+++ src/components/userSearch/search.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import "../../assets/style/profile/search.css";
 
-function Search() {
+function Search({ nickname, imge }) {
+  const imgSrc = "https://minitwit-sinabro.s3.ap-northeast-2.amazonaws.com/";
   return (
-    <div>
-      <input type="text" placeholder="친구를 찾아보세요" />
+    <div className="searchLayout">
+      <div className="userList">
+        <img src={imgSrc + imge} />
+        <h2>{nickname}</h2>
+      </div>
     </div>
   );
 }
+
+export default Search;
