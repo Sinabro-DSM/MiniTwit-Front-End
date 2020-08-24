cf4f474825fcb5be95d2ab50ec8228dd05157e60
Srim <deamoon0801@naver.com>
2020-08-25 03:54:09 +0900

    Login 구현

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import '../../assets/style/login/login.css'
+import '../../assets/style/login/login.css';
+import axios from 'axios';
 
 const Login = () => {
+
+    const onSubmitLogin = () => {
+        const email = document.getElementsByName("email")[0].value.trim();
+        const password = document.getElementsByName("password")[0].value.trim();
+        console.log(email);
+        console.log(password);
+        const data = {
+            email : email,
+            password : password
+        }
+
+        axios.post('http://3.34.198.6:3000/user/login',data)
+        .then((res) => {
+            console.log(res)
+            localStorage.setItem('accessToken', res.data.accessToken);
+            localStorage.setItem('refreshToken', res.data.refreshToken);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
     return (
         <div className="App">
             <header className="loginHeader">
@@ -12,17 +34,17 @@ const Login = () => {
                 <form>
                     <div className="idContainer">
                         <label for="inputId" className="idLabel">이메일</label><br></br>
-                        <input type="email" className="inputId"></input><br></br>
+                        <input type="email" className="inputId" name="email"></input><br></br>
                     </div>
                     <div className="blank"></div>
                     <div className="pwContainer">
                         <label for="inputPw" className="pwLabel">비밀번호</label><br></br>
-                        <input type="password" className="inputPw"></input>
+                        <input type="password" className="inputPw" name="password"></input>
                     </div>
                 </form>
             </div>
             <div>
-                <input type="button" className="summitBtn" value="로그인"></input>
+                <button className="submitBtn" onClick={onSubmitLogin}>로그인</button>
                 <p className="signUp">트위터 가입하기</p>
             </div>
         </div>
