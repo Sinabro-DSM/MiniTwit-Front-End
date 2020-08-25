b0dd3f432adc1ba1df3e6ed74dce4e3f412e1db9
Srim <deamoon0801@naver.com>
2020-08-26 00:55:10 +0900

    post ui

--- src/components/main/PostItem.js
+++ src/components/main/PostItem.js
@@ -5,17 +5,19 @@ import '../../assets/style/main/main.css'
 const PostItem = () => {
     return (
         <div className="postItem">
-            <div className="infoBox">
-                <div className="userNameBox">
-                    <img src={profile}></img>
-                    <h3>nickname</h3>
-                    <span>@asdfasdf</span>
+            <img src={profile}></img>
+            <div className="contentsContainer">
+                <div className="infoBox">
+                    <div className="userNameBox">
+                        <h3>nickname</h3>
+                        <span>@asdfasdf</span>
+                    </div>
+                        <button>X</button>
                 </div>
-                    <button>X</button>
-            </div>
-            <div className="contentsBox">
-                <p>어쩌구 저쩌구</p>
-                <img src={profile}></img>
+                <div className="contentsBox">
+                    <p>어쩌구 저쩌구</p>
+                    <img src={profile}></img>
+                </div>
             </div>
         </div>
     );
