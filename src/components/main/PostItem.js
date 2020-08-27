32e3e628fcd96ad08009d9f6b5feb889a457e9cf
Srim <deamoon0801@naver.com>
2020-08-28 02:55:25 +0900

    post delete

--- src/components/main/PostItem.js
+++ src/components/main/PostItem.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import axios from 'axios'
 import profile from '../../assets/img/profile.PNG'
 import '../../assets/style/main/main.css'
 
-const PostItem = ({email,nickname,userImg,content,uploadImg}) => {
+const PostItem = ({id, email,date,nickname,userImg,content,uploadImg = []}) => {
+    const onRemove = () => {
+        const config = {
+            headers : {'access-token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRmOTg0NGFhMmZmMjA1M2E5NjAxMjYzMTIxOWRiNWI5IiwiZW1haWwiOiJzZXVuZ2Jpbjk4NTBAZ21haWwuY29tIiwibmlja25hbWUiOiJ5c2IiLCJpYXQiOjE1OTg1NTAxMzUsImV4cCI6MTU5ODU1MTkzNX0.tNvNE8qhdNEefeSg1vqqc5buZaaeyGxQxunPgDagcoI'}
+        }
+        const res = axios.delete("http://52.78.186.198:3000/timeline/" + id, config)
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="postItem">
             <img src={userImg}></img>
             <div className="contentsContainer">
                 <div className="infoBox">
+                    <div className="infoInnerBox">
                         <h3>{nickname}</h3>
-                    <div className="userNameBox">
-                        <span>{email}</span>
+                        <div className="userNameBox">
+                            <span>{email}</span>
+                            <span>{date}</span>
+                        </div>
                     </div>
-                        <button>X</button>
+                        <button onClick={onRemove}>X</button>
                 </div>
                 <div className="contentsBox">
                     <p>{content}</p>
