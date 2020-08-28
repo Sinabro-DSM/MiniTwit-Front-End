15a327abc3bbebedf7762cfb419e635d75b2f25e
Srim <deamoon0801@naver.com>
2020-08-29 00:17:21 +0900

    post like post delete

--- src/components/main/PostItem.js
+++ src/components/main/PostItem.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios'
 import profile from '../../assets/img/profile.PNG'
 import '../../assets/style/main/main.css'
+import like from "../../assets/img/like.png"
+import unlike from "../../assets/img/unlike.png"
 
-const PostItem = ({id, email,date,nickname,userImg,content,uploadImg = []}) => {
-    const onRemove = () => {
-        const config = {
-            headers : {'access-token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRmOTg0NGFhMmZmMjA1M2E5NjAxMjYzMTIxOWRiNWI5IiwiZW1haWwiOiJzZXVuZ2Jpbjk4NTBAZ21haWwuY29tIiwibmlja25hbWUiOiJ5c2IiLCJpYXQiOjE1OTg1NTAxMzUsImV4cCI6MTU5ODU1MTkzNX0.tNvNE8qhdNEefeSg1vqqc5buZaaeyGxQxunPgDagcoI'}
+const PostItem = ({id, email,date,nickname,userImg,content,isLike,isMine,uploadImg = []}) => {
+    let deleteButtonStyle = "";
+    let likeButton = "";
+    
+    if(isMine === false)
+    {
+        deleteButtonStyle = "none"
+    }
+
+    if(isLike === true)
+    {
+        likeButton = like;
+    }
+    else
+    {
+        likeButton = unlike;
+    }
+
+    const config = {
+        headers : {'access-token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ3MzE1Y2Y1NjBhYWQ5ZjRiYzFkMWQxNzEwNzkyNjkwIiwiZW1haWwiOiJzZXVuZ2Jpbjk4NTBAZ21haWwuY29tIiwibmlja25hbWUiOiJ5c2IiLCJpYXQiOjE1OTg2MjYwMjgsImV4cCI6MTU5ODYyNzgyOH0.lXtrjrFzpgAKZfCqAGLd33dlATrNEQJHD3wwGw2di0M'}
+    }
+
+    const onRemove = async () => {
+        await axios.delete("http://52.78.186.198:3000/timeline/" + id, config)
+        setTimeout(function() {
+            window.location.reload();
+          }, 300);
+    }
+
+    const onSubmitLike = () => {
+        if(isLike === false)
+        {
+            axios.get("http://52.78.186.198:3000/timeline/like/" + id, config)
+            setTimeout(function() {
+                window.location.reload();
+              }, 300);
         }
-        const res = axios.delete("http://52.78.186.198:3000/timeline/" + id, config)
-        .then((res) => {
-            console.log(res)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+        else
+        {
+            axios.delete("http://52.78.186.198:3000/timeline/like/" + id, config)
+            setTimeout(function() {
+                window.location.reload();
+              }, 300);
+        }
     }
 
     return (
@@ -29,11 +63,12 @@ const PostItem = ({id, email,date,nickname,userImg,content,uploadImg = []}) => {
                             <span>{date}</span>
                         </div>
                     </div>
-                        <button onClick={onRemove}>X</button>
+                        <button style={{display : deleteButtonStyle}} onClick={onRemove}>X</button>
                 </div>
                 <div className="contentsBox">
                     <p>{content}</p>
                     <img src={uploadImg}></img>
+                        <img className="likeButton" src={likeButton} onClick={onSubmitLike}></img>
                 </div>
             </div>
         </div>
