ffbb94877752c561bf535724316efe4851502aeb
성동명 <sungdm0303@gmail.com>
2020-09-01 21:56:48 +0900

    signUp 구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SignUp from './components/signUp/SignUp'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import SignUpCheck from './components/signUp/SignUpCheck';
+import SignUpSuccess from './components/signUp/SignUpSuccess';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={SignUp}></Route>
+        <Route path="/SignUpCheck" component={SignUpCheck}></Route>
+        <Route path="/SignUpSuccess" component={SignUpSuccess}></Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
