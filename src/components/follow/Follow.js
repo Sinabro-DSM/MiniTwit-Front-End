98a01b660a2e4239d290a51f03250c30f284c555
Haerin <kim34458747@gmail.com>
2020-09-03 00:28:06 +0900

    feed like 추가

--- src/components/follow/Follow.js
+++ src/components/follow/Follow.js
@@ -23,15 +23,23 @@ class Follow extends Component {
   } 
 
   async componentDidMount() {
-    const resProfile = await axios.get("http://13.209.47.153:3000/profile/" + this.state.id, this.userToken);
-    this.setState({ viewFeed: resProfile.data.profile.Timelines, userEmail: resProfile.data.profile.email, userName: resProfile.data.profile.nickname, userImg: resProfile.data.profile.img});
+    const resProfile = await axios.get( "http://54.180.103.146:3000/profile/" + this.state.id, this.userToken);
+    this.setState
+    ({ 
+      viewFeed: resProfile.data.profile.Timelines, 
+      userEmail: resProfile.data.profile.email,
+      userName: resProfile.data.profile.nickname, 
+      userImg: resProfile.data.profile.img,
+      followerValue: resProfile.data.profile.Followers.length,
+      followingValue: resProfile.data.profile.Followings.length,
+    });
   
   }
 
   userToken = {
     headers: {
       "access-token": 
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ4YjA1NWE2NTZlMTE1ODg4NDJjNGMyNzBiZjU3Nzg2IiwiZW1haWwiOiJzZXVuZ2Jpbjk4NTBAZHNtLmhzLmtyIiwibmlja25hbWUiOiJuaWNrIiwiaWF0IjoxNTk4OTY5NDQ1LCJleHAiOjE1OTkwNTU4NDV9.FR4gZdD7ijpflfrd1jPeAVsGky81fBtp9yLS1qZIGRA",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ4YjA1NWE2NTZlMTE1ODg4NDJjNGMyNzBiZjU3Nzg2IiwiZW1haWwiOiJzZXVuZ2Jpbjk4NTBAZHNtLmhzLmtyIiwibmlja25hbWUiOiJuaWNrIiwiaWF0IjoxNTk5MDYwMDgyLCJleHAiOjE1OTkxNDY0ODJ9.ogkGqcypBK6gQQE_19zfAKy2wH6OpgiNtiJNWe2IqIo",
     }
   }
 
@@ -46,18 +54,18 @@ class Follow extends Component {
 
   following() {
     const res = axios.get('http://13.209.47.153:3000/follow/' + this.state.id, this.followToken);
-    this.setState({isFollow: true, followerValue: this.state.followerValue + 1});
+    this.setState({isFollow: true});
   }
 
   unFollow() {
     const res = axios.delete('http://13.209.47.153:3000/follow/' + this.state.id, this.followToken);
-    this.setState({isFollow: false, followerValue: this.state.followerValue - 1})
+    this.setState({isFollow: false})
   }
 
   
    
   render() {
-    const { viewFeed, userEmail, userName, userImg }= this.state;
+    const { viewFeed, userEmail, userName, userImg, followerValue, followingValue}= this.state;
      ;
     return (
       <React.Fragment>
@@ -68,25 +76,26 @@ class Follow extends Component {
            ) : (
             <button className="following" onClick={this.following}>팔로우</button>
         )}
-          {/* <span id="otherId">{viewProfile.nickname}</span>
-           <span id="otherEmail">{viewProfile.email}</span> */}
+          <span id="otherId">{userName}</span>
+           <span id="otherEmail">{userEmail}</span>
         <div className="followers">
-        <span>{this.state.followerValue} 팔로워</span>
-        <span>{this.state.followingValue} 팔로잉</span>
+        <span>{followerValue} 팔로워</span>
+        <span>{followingValue} 팔로잉</span>
         </div>
         </div>
 
         <div>
           {viewFeed.map((view) => (
             <UserFeed
+              id = {view.id}
               key={view.id} 
               user={view.userId}
-              userImg={userImg}
-              email={userEmail}
+               email={userEmail}
               content={view.content}
               imges={view.Images}
               nickname={userName}
-
+              isLike={view.isLike}
+              userImg={userImg}
             />
           ))}
         </div>
--- src/components/profile/UserFeed.js
+++ src/components/profile/UserFeed.js
@@ -1,12 +1,42 @@
 import React from "react";
 import '../../assets/style/profile/userFeed.css';
+import like from '../../assets/img/like.png'
+import unlike from '../../assets/img/unlike.png';   
+import axios from "axios";
 
-function UserFeed({user, email, content, userImg, imges=[], nickname }) {
+function UserFeed({id, isLike, user, email, content, userImg, imges=[], nickname }) {
     const fixUrl = "https://minitwit-sinabro.s3.ap-northeast-2.amazonaws.com/";
+    const likeUrl = "http://54.180.103.146:3000/timeline/like/";
+    let likeButton = "";
+
+    if(isLike === false) {
+        likeButton = unlike;
+    }
+    else {
+        likeButton = like;
+    }
+    console.log(isLike);
+    const config = {
+        headers: {
+            'access-token':
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ4YjA1NWE2NTZlMTE1ODg4NDJjNGMyNzBiZjU3Nzg2IiwiZW1haWwiOiJzZXVuZ2Jpbjk4NTBAZHNtLmhzLmtyIiwibmlja25hbWUiOiJuaWNrIiwiaWF0IjoxNTk5MDYwMDgyLCJleHAiOjE1OTkxNDY0ODJ9.ogkGqcypBK6gQQE_19zfAKy2wH6OpgiNtiJNWe2IqIo",
+        }
+    }
+    const onLike = () => {
+        if(isLike === false) {
+            axios.get(likeUrl + id, config);
+        }
+        else {
+            axios.delete(likeUrl + id, config);
+        }
+    }
+
     return(
         <div className="otherFeed">
             <div className="otherFeedList">
-                <img id="userImge" src={fixUrl + userImg} />
+                <div className="userProfile">
+                <img src={fixUrl + userImg} />
+                </div>
                 <span id="nickname">{nickname}</span>
                 <span id="email">{email}</span>
                 <span id="content">{content}</span>
@@ -15,6 +45,7 @@ function UserFeed({user, email, content, userImg, imges=[], nickname }) {
                 <img key={img.id} src={fixUrl + img.img}/>
                 ))}
                 </div>
+                <img className="likeBtn" src={likeButton} onClick={onLike}></img>
             </div>
         </div>
     );
