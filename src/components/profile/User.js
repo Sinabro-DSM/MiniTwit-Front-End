67326801bd7f2c79fdc3c743ef89ed23f0aef77e
Haerin <kim34458747@gmail.com>
2020-09-04 00:01:57 +0900

    수정

--- src/components/profile/User.js
+++ src/components/profile/User.js
@@ -1,20 +1,88 @@
-
-import React from "react";
+import React, { Component } from "react";
 import "../../assets/style/profile/profile.css";
+import '../profile/ProfileFeed';
+import ProfileEdit from "./ProfileEdit";
+import ProfileFeed from "../profile/ProfileFeed";
+import axios from "axios";
 
-function User() {
-  return (
-    <div className="userContainer">
-      <div className="background"></div>
-      <button id="editBtn">프로필 설정하기</button>
-      <div className="profilePicture"></div>
-      <div className="profile">
-        <p id="id">rin3583</p>
-        <p id="email">@user.email.com</p>
-        <a>0 팔로워</a>
-        <a>0 팔로잉</a> 
-      </div>
-    </div>
-  );
+class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false,
+      profileFeed: [],
+      profileName: '',
+      profileImg: '',
+      profileEmail: '',
+      follower: 0,
+      following: 0,
+    };
+  }
+  config = {
+    headers: {
+      'access-token': 
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ4YjA1NWE2NTZlMTE1ODg4NDJjNGMyNzBiZjU3Nzg2IiwiZW1haWwiOiJzZXVuZ2Jpbjk4NTBAZHNtLmhzLmtyIiwibmlja25hbWUiOiJuaWNrIiwiaWF0IjoxNTk5MTQxNjUwLCJleHAiOjE1OTkyMjgwNTB9.SObMyr7J5LqI2H5zPeM82xqTwG_SlQQhmZVYOX3HxZ8",
+    }
+  }
+  async componentDidMount() {
+    const res = await axios.get("http://54.180.103.146:3000/profile/", this.config);
+    this.setState({ 
+      profileFeed: res.data.profile.Timelines,
+      profileName: res.data.profile.nickname,
+      profileImg: res.data.profile.img,
+      profileEmail: res.data.profile.email,
+      follower: res.data.profile.Followers.length,
+      following: res.data.profile.Followings.length,
+    })
+  }
+
+  open = () => {
+    this.setState({ modalOpen: true });
+  };
+
+  close = () => {
+    this.setState({ modalOpen: false });
+  };
+  render() {
+    const imgUrl = "https://minitwit-sinabro.s3.ap-northeast-2.amazonaws.com/";
+    const { profileFeed, profileName, profileImg, profileEmail, follower, following} = this.state;
+    return (
+      <div className="userContainer">
+        <div className="background"></div>
+        <button className="editBtn" onClick={this.open}>
+          프로필 설정하기
+        </button>
+        <ProfileEdit isOpen={this.state.modalOpen} close={this.close} />
+        <div className="profilePicture" src={imgUrl+profileImg}></div>
+        <div className="profile">
+          <span id="name">{profileName}</span>
+          <span id="email">{profileEmail}</span>
+          <div className="follow">
+          <span>{follower} 팔로워</span>
+          <span>{following} 팔로잉</span>
+        </div>
+        </div>
+        
+        <div>
+          {profileFeed.map((feed) => (
+              <ProfileFeed
+              key={feed.id}
+              id={feed.id}
+              content={feed.content}
+              email={profileEmail}
+              name={profileName}
+              imges={feed.Images}
+              isLike={feed.isLike}
+              profileImg={profileImg}
+              
+              />
+          ))}
+        </div>
+        </div>
+
+        
+    );
+  }
+}
 
 export default User;
