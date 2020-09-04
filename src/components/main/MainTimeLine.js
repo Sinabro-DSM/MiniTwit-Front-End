0a328578ef3f0030ef14cd496340aa86d586ec0b
Srim <deamoon0801@naver.com>
2020-09-05 01:07:20 +0900

    Sidebar 및 Header컴포넌트 추가

--- src/components/main/MainTimeLine.js
+++ src/components/main/MainTimeLine.js
@@ -1,7 +1,16 @@
 import React from "react";
+import TImeLineView from './TimeLineView'
+import Sidebar from '../Sidebar/Sidebar'
+import Header from '../header/Header'
 
 const MainTimeLine = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Sidebar></Sidebar>
+      <Header></Header>
+      <TImeLineView></TImeLineView>
+    </div>
+  );
 };
 
 export default MainTimeLine;
--- src/components/profile/OtherUser.js
+++ src/components/profile/OtherUser.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "../../assets/style/profile/profile.css";
 import Follow from '../follow/Follow';
+import Sidebar from "../Sidebar/Sidebar";
+import Hedaer from "../header/Header"
 
 function OtherUser(props) {
   const {id} = props.match.params
     return (
-      <div className="userContainer">
-        <div className="background"></div>
-        <Follow id={id}/>
+      <div>
+        <Sidebar></Sidebar>
+        <Hedaer></Hedaer>
+        <div className="userContainer">
+          <div className="background"></div>
+          <Follow id={id}/>
+        </div>
       </div>
+     
     );
   }
 
